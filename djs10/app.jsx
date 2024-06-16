1bf4a7d38945cb61069001fcc36fa68d185a2f80
Valentia Puseletso Kgolane <156904482+VPKGO414@users.noreply.github.com>
2024-06-16 23:13:01 +0200

    Add files via upload

---
+++ djs10/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BlogPosts from './BlogPosts';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BlogPosts />
+    </div>
+  );
+};
+
+export default App;
